0acffc480926852d46cf1e2e6b33f3d863391343
jhabib <mjawadhabib@gmail.com>
2016-02-08 23:32:13 -0800

    added histograms and added answers for part 2.

--- W203 Week 5/Lab 1/asinh_trans.R
+++ W203 Week 5/Lab 1/asinh_trans.R
@@ -1,3 +1,6 @@
+# origin of the code below
+# http://stackoverflow.com/questions/14504869/histogram-with-negative-logarithmic-scale-in-r
+
 library(ggplot2)
 library(scales)
 
@@ -13,3 +16,4 @@ asinh_trans <- function(){
 ggplot(demo,aes(x,x))+geom_point(size=2)+
   scale_y_continuous(trans = 'asinh',breaks=c(-100,-50,-10,-1,0,1,10,50,100))+
   theme_bw()
+# Further review needed
--- W203 Week 5/async_material_week5.R
+++ W203 Week 5/async_material_week5.R
@@ -1,8 +1,6 @@
 library(ggplot2)
 
-load("Countries")
-
-setwd("C:/Users/SP4/Google Drive/Exploring and Analyzing Data/W203 Async/W203 Week 5/")
+setwd("C:/Users/SP4/Documents/Exploring and Analyzing Data/W203 Async/W203 Week 5/")
 getwd()
 
 library(datasets)
@@ -28,3 +26,49 @@ View(Fertility)
 
 Countries <- merge(Countries, Fertility, by = "Country", all = TRUE)
 View(Countries)
+
+write.csv(Countries, "Countries.csv")
+
+summary(Countries)
+
+# demonstration of histogram
+graph1 <- ggplot(Countries, aes(gdp))
+graph1 + geom_histogram()
+
+graph1 + geom_histogram() + scale_x_log10()
+
+graph1 + geom_histogram(binwidth = .2) + scale_x_log10()
+
+
+# investigation of corruption and growth of internet users
+Corruption <- read.csv("2012_Corruption_Index.csv")
+View(Corruption)
+summary(Corruption)
+
+Countries <- merge(Countries, Corruption, by = "Country", all = TRUE)
+View(Countries)
+
+Countries$internet_growth <- (Countries$internet_users_2011 - Countries$internet_users_2010)/(Countries$internet_users_2010)
+
+graph2 <- ggplot(Countries, aes(internet_growth))
+
+graph2 + geom_histogram()
+
+graph3 <- ggplot(Countries, aes(cpi, internet_growth))
+graph3 + geom_point(shape = 4)
+graph3 + geom_point() + scale_y_log10()
+
+# recode corruption index as a binary variable
+Countries$high_cpi <- Countries$cpi > mean(Countries$cpi, na.rm = TRUE)
+Countries$high_cpi <- ifelse(Countries$cpi > mean(Countries$cpi, na.rm = TRUE), "Trustworthy", "Corrupt")
+Countries$high_cpi
+
+graph4 <- ggplot(Countries[!is.na(Countries$high_cpi),], aes(high_cpi, internet_growth))
+graph4 + geom_boxplot()
+
+graph4 +  stat_summary(fun.y = mean, 
+                       geom = "bar", 
+                       fill = "White", 
+                       colour = "Black") + stat_summary(fun.data = mean_cl_normal, 
+                                                        geom = "errorbar", 
+                                                        width = 0.2)
