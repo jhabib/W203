e5fdcdadd571e43db82a1ed8f40c72979e21f69e
Jawad Habib <Jawad Habib>
2016-02-22 22:57:29 -0800

    finished Type II error calculation

--- W203 Week 6/w203_week6_supp2.R
+++ W203 Week 6/w203_week6_supp2.R
@@ -1,4 +1,4 @@
-# Write a function in R that does the following:
+# 1. Write a function in R that does the following:
 # Takes sample data (e.g., as a vector), mean of the null hypothesis,  
 # population standard deviation,  a Boolean variable indicating whether we want a one-tailed or two-tailed test, 
 # and another Boolean variable indicating which tail (left or right) should be taken in case of a one-tailed test.
@@ -15,36 +15,98 @@ myfun <- function(data, mu, pop.sd, two.tailed=TRUE, left.tail=NULL) {
   # calculate z-score
   z.raw <- (mean(data) - mu) / (pop.sd/(sqrt(length(data))))
   
-  # calculate a p-value assuming a right-tailed test
-  p.val <- pnorm(z.raw, lower.tail = FALSE)
-  
   # convert p-value to two-tailed if applicable
   if(two.tailed) {
-    p.val <- 2*pnorm(abs(z.raw), lower.tail = FALSE)
+    p.val <- 2*pnorm(-abs(z.raw), lower.tail = TRUE)
+    rejectNull <- ifelse(p.val < 0.025, TRUE, FALSE)
   } else {
     if(left.tail == TRUE) { 
-      p.val <- pnorm(z.raw, lower.tail = TRUE)
+      p.val <- pnorm(-abs(z.raw), lower.tail = TRUE)
+    } else { # no need to caclualte separate p.vals for left- and right- but we'll do it anyways
+      p.val <- pnorm(abs(z.raw), lower.tail = FALSE)
     }
+    rejectNull <- ifelse(p.val < 0.05, TRUE, FALSE)
   }
   
-  # create a boolean value for test pass or fail
-  rejectNull <- ifelse(p.val < 0.05, TRUE, FALSE)
-  
-  return(list(p.val, rejectNull))
+  (list("p.value" = p.val, "rejectNull" = rejectNull))
 }
 
+set.seed(5000)
 
-# Choose a sample size and then use rnorm(n, mean, sd) to generate a random sample 
+# 2. Choose a sample size and then use rnorm(n, mean, sd) to generate a random sample 
 # and test your function.
-test_mean <- 12.345
-test_sd <- 2.32
-test_data <- rnorm(100, mean = test_mean, sd = test_sd)
+pop_mean <- 50
+pop_sd <- 21
+sample_size <- 36
+sample_data <- rnorm(sample_size, mean = pop_mean, sd = pop_sd)
 
 # run a two-tailed test
-myfun(test_data, mu = test_mean, pop.sd = test_sd, two.tailed = TRUE, left.tail = NULL)
+myfun(sample_data, mu = pop_mean, pop.sd = pop_sd, two.tailed = TRUE, left.tail = NULL)
 
 # run a left-tailed test
-myfun(test_data, mu = test_mean, pop.sd = test_sd, two.tailed = FALSE, left.tail = TRUE)
+myfun(sample_data, mu = pop_mean, pop.sd = pop_sd, two.tailed = FALSE, left.tail = TRUE)
 
 # run a right tailed test
-myfun(test_data, mu = test_mean, pop.sd = test_sd, two.tailed = FALSE, left.tail = FALSE)
+myfun(sample_data, mu = pop_mean, pop.sd = pop_sd, two.tailed = FALSE, left.tail = FALSE)
+
+# 3. Use replicate or sapply (or replicate) to generate a sample 
+# and do the test multiple times (say 1000). 
+# Plot the histogram of p-values that you are getting when the Null is true. 
+# Is your function calculating Type-I errors correctly?
+
+num.trials <- 1000
+
+result.vec.two.tailed <- replicate(num.trials, 
+                                   myfun(rnorm(sample_size, mean = pop_mean, sd = pop_sd), 
+                                         mu = pop_mean, 
+                                         pop.sd = pop_sd, 
+                                         two.tailed = TRUE, 
+                                         left.tail = NULL)$p.value, 
+                                   simplify = TRUE)
+hist(result.vec.two.tailed)
+
+result.vec.left.tailed <- replicate(num.trials, 
+                                   myfun(rnorm(sample_size, mean = pop_mean, sd = pop_sd), 
+                                         mu = pop_mean, 
+                                         pop.sd = pop_sd, 
+                                         two.tailed = FALSE, 
+                                         left.tail = TRUE)$p.value, 
+                                   simplify = TRUE)
+hist(result.vec.left.tailed)
+
+
+result.vec.right.tailed <- replicate(num.trials, 
+                                    myfun(rnorm(sample_size, mean = pop_mean, sd = pop_sd), 
+                                          mu = pop_mean, 
+                                          pop.sd = pop_sd, 
+                                          two.tailed = FALSE, 
+                                          left.tail = FALSE)$p.value, 
+                                    simplify = TRUE)
+hist(result.vec.right.tailed)
+
+# 4. Now assume your Null is false. Note: For type-II error calculation, 
+# you need a specific assumption about the mean of the population from which the sample is taken. 
+# You can assume that this mean is one tenth of one standard deviation above the Null mean.
+# Calculate type-II errors both theoretically and by simulation as in step 3.
+
+# test_data represents our test vector with population mean = pop_mean and population sd = pop_sd
+# we assume that mean(test_data) is our null hypothesis in this case
+
+# Any values above this rejection.mean will cause us to reject the null hypothesis
+rejection.mean <- pop_mean + pop_sd/10
+# For original population with mean pop_mean, rejection.mean represents a z-score of
+z.pop <- (rejection.mean - pop_mean) / (pop_sd/sqrt(length(sample_data)))
+
+
+# Assuming that Null (pop_mean) is False, we take mean(sample_data) as our Null hypothesis
+# That is, we use a distribution with mean(sample_data)
+z.type.one <- (rejection.mean - mean(sample_data))/(pop_sd/sqrt(length(sample_data)))
+p.type.one <- pnorm(abs(z.type.one), lower.tail = FALSE)
+
+
+# Type II error is the area to the left of rejection.mean on the original distribution
+# with mean = pop.mean
+p.type.two <- pnorm(z.type.one)
+test.power <- 1 - p.type.two
+# There is a probability = p.type.two that we will fail to reject the null hypothesis when we should
+
