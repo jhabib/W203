490c322926fe9a48f49f73bd84339403ba186715
Jawad Habib <mjawadhabib@gmail.com>
2016-03-03 20:07:06 -0800

    following class material

--- W203 Week 9/w203_week9_async.R
+++ W203 Week 9/w203_week9_async.R
@@ -31,3 +31,51 @@ cor(examData$Exam, examData$Anxiety, use = "pairwise.complete.obs", method = "ke
 # rcorr(examData[1:4], type = "pearson")
 rcorr(as.matrix(examData[, 1:4]), type = "pearson")
 
+cor.test(examData$Exam, examData$Anxiety, method = "pearson")
+
+cor.test(examData$Exam, examData$Anxiety, alternative = "less", method = "pearson")
+
+cor.test(examData$Exam, examData$Anxiety, alternative = "less", method = "pearson", conf.level = 0.99)
+
+examData2 <- examData[, c("Exam", "Anxiety", "Revise")]
+cor(examData2)
+
+cor(examData[, c("Exam", "Anxiety", "Revise")])
+
+examMatrix <- as.matrix(examData[, c("Exam", "Anxiety", "Revise")])
+
+rcorr(examMatrix)
+
+cor.test(examData$Anxiety, examData$Exam)
+
+cor(examData2)^2
+
+cor(examData2)
+
+cor(examData2)^2*100
+
+liarData <- read.delim("The Biggest Liar.dat", header = TRUE)
+
+liarData = read.delim(file.choose(), header = TRUE)
+
+cor(liarData$Position, liarData$Creativity, method = "spearman")
+
+library(foreign)
+
+insurgency.stata <- read.dta("lyall2010.dta")
+
+
+View(insurgency.stata)
+
+unique(insurgency.stata$occ)
+unique(insurgency.stata$wdl)
+
+summary(insurgency.stata)
+
+
+cor(insurgency.stata$occ, insurgency.stata$wdl)
+
+
+aggregate(insurgency.stata$wdl ~ insurgency.stata$occ, data = insurgency.stata, FUN = length)
+
+
