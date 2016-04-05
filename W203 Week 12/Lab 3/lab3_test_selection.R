30f3f1ab8bd74232ae6146ad6c4184d976799081
jhabib <mjawadhabib@gmail.com>
2016-04-04 23:49:10 -0700

    Lab 3

---
+++ W203 Week 12/Lab 3/lab3_test_selection.R
@@ -0,0 +1,67 @@
+s <- summary(GSS$income91)
+class(s)
+hist(s, freq = FALSE, breaks = 10)
+?hist
+
+income.art <- GSS[GSS$visitart == "Yes", 14]
+hist(summary(income.art), freq = FALSE, breaks = 10)
+
+
+income.notart <- GSS[GSS$visitart == "No", 14]
+hist(summary(income.notart), freq = FALSE, breaks = 10)
+
+names(GSS)
+
+summary(GSS$country)
+summary(GSS$age)
+
+cmusic <- data.frame(GSS$age, GSS$country)
+names(cmusic) <- c("age", "country")
+levels(cmusic$country)
+hist(cmusic$age)
+
+library(car)
+leveneTest(cmusic$age ~ cmusic$country)
+cmusic$age
+?aov
+
+amodel <- aov(age ~ country, data=cmusic)
+amodel
+summary(amodel)
+
+GSS$relig
+levels(GSS$sex)
+GSS$sibs
+
+unique(GSS$sibs)
+
+hist(GSS$educ[GSS$educ < 98], breaks = 20)
+hist(GSS$tvhours[GSS$tvhours < 99])
+summary(GSS$tvhours)
+
+plot(GSS$educ, GSS$tvhours)
+qqplot(GSS$educ, GSS$tvhours)
+
+
+levels(GSS$politics)
+levels(GSS$news)
+hist(summary(GSS$politics))
+hist(summary(GSS$news))
+
+
+unique(GSS$age)
+levels(GSS$country)
+cmusic.complete <- cmusic[cmusic$country != c("NA", "NAP"),]
+
+library(stats)
+
+cmodel <- kruskal.test(age ~ country, data = cmusic.complete, na.action = na.exclude)
+summary(cmodel)
+
+cmusic.complete$ranks <- rank(cmusic.complete$age)
+by(cmusic.complete$ranks, cmusic.complete$country, mean)
+
+boxplot(cmusic.complete$age ~ cmusic.complete$country, na.action = na.exclude)
+?boxplot
+
+unique(cmusic.complete$country)
