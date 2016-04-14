a521f281c73f30ac4ea5aefb9783aefea9dde78a
jhabib <mjawadhabib@gmail.com>
2016-04-13 21:19:44 -0700

    faraway ch 12

---
+++ W203 Week 13/faraway_insurance_ch12.R
@@ -0,0 +1,51 @@
+getwd()
+# setwd("W203 Week 13)
+
+install.packages("faraway")
+
+library(faraway)
+data(eco)
+plot(income ~ usborn, data = eco)
+iub <- lm(income ~ usborn, data = eco)
+summary(iub)
+plot(income ~ usborn, data = eco, xaxs = "i")
+abline(iub$coefficients)
+
+# chicago data
+data(chicago)
+head(chicago)
+
+ch <- data.frame(chicago[, c(1:4, 6)], income = chicago[ ,7]/1000)
+head(ch)
+summary(ch)
+
+par(mfrow = c(2, 3))
+for (i in 1:6) hist(ch[, i], main = names(ch)[i])
+for (i in 1:6) boxplot(ch[, i], main = names(ch)[i])
+pairs(ch)
+
+?pairs
+
+summary(lm(involact ~ race, data = ch))
+
+g <- lm(involact ~ race + fire + theft + age + log(income), data = ch)
+summary(g)
+
+plot(g$fitted.values, g$residuals)
+abline(h = 0)
+qqnorm(g$residuals)
+
+gi <- lm.influence(g)
+for (i in 1:5) qqnorml(gi$coefficients[, i+1], main = names(ch)[-5][1])
+
+qqnorml(rstudent(g), main = "Jacknife Residuals")
+qt(0.05/(2*47), 47-6-1)
+
+halfnorm(cooks.distance(g), main = "Cook's Distance")
+
+ch[c(6, 24), ]
+
+
+g2 <- lm(involact ~ race + fire + theft + age + log(income), data = ch, subset = (1:47)[-c(6, 24)])
+summary(g2)
+
