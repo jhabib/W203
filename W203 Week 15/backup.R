e722a4788fa7b7ad687666111b87b0773a10caac
Jawad Habib <mjawadhabib@gmail.com>
2016-04-27 15:02:18 -0700

    final update

---
+++ W203 Week 15/backup.R
@@ -0,0 +1,41 @@
+---
+  title: "W203 Final"
+author: "Mohammad Jawad Habib"
+date: "April 21, 2016"
+output: pdf_document
+---
+  
+  # Part 1: Multiple Choice
+  Q1. b, Ordinary Least Squares regression
+
+Q2. b, All coefficients for each independent variable equal zero
+
+Q3. a, There are statistically significant relationships between each
+independent variable and the dependent variable
+
+Q4. b, Maximum number of pushups in 3 minutes (because it might be correlated
+                                               with tricep strength which is already in our model)
+
+Q5. d, because we did not ourselves subject people to this 
+torment (um, treatment); we assume that the populations of the two CA counties 
+do not differ substantially prior to the ISP ban on dating sites
+
+Q6. c, Regression assumptions that have been met
+
+Q7. b, The power of the test (is not a random variable, it's calculated)
+                              
+                              Q8. c, Insignificant results are commonly relegated to the file drawer
+                              If this were actually true, only 5% or less of the publish research would 
+                              not be reproducible. So relegating insignificant research to the file drawer 
+                              does not help to explain why more than 5% of the published results appear to 
+                              be type-1 errors
+                              
+                              # Part 2. Test Selection
+                              Q9. a, chi-square (use_reddit ~ marital_status)
+                              
+                              Q10. d, ANOVA (life_quality ~ region)
+                              
+                              Q11. d, ANOVA (years_in_relationship ~ flirted_online)
+                              
+                              Q12. 
+                              
